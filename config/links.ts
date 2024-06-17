22dc320dff844f6c5c723f3d4162438061dee726
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2024-06-17 17:02:24 +0900

    chore: set app redirect url to /KayenSwap

--- config/links.ts
+++ config/links.ts
@@ -1,6 +1,6 @@
-export const DISCORD_LINK = "https://discord.gg/2GxuJvbQA6"
-export const X_LINK = "https://twitter.com/KAYEN_Protocol"
-export const MEDIUM_LINK = "https://medium.com/@kayen-protocol"
-export const GIUTUB_LINK = "https://github.com/Kayen-Protocol/"
-export const SWAP_PRODUCT_LINK = "https://app.kayen.org/"
-export const DOCS_LINK = "https://kayen-protocol.gitbook.io/documentation"
+export const DISCORD_LINK = "https://discord.gg/2GxuJvbQA6";
+export const X_LINK = "https://twitter.com/KAYEN_Protocol";
+export const MEDIUM_LINK = "https://medium.com/@kayen-protocol";
+export const GIUTUB_LINK = "https://github.com/Kayen-Protocol/";
+export const SWAP_PRODUCT_LINK = "https://app.kayen.org/KayenSwap/";
+export const DOCS_LINK = "https://kayen-protocol.gitbook.io/documentation";
