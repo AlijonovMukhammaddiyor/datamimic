9a3244f1a80228f550d03ba4434e706c1210ae11
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2024-08-21 13:25:23 +0900

    remove what we do section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import AssetSection from "@/components/AssetSection";
 import Footer from "@/components/Footer";
 import GameSection from "@/components/GameSection";
@@ -18,65 +18,67 @@ export default function Home() {
     setMounted(true);
   }, []);
 
-  if(!mounted) {
-    return null
+  if (!mounted) {
+    return null;
   }
-  return (
-    isMobile ? 
-      <>
-          <div className='flex justify-center w-full h-[60px]'>
-            <div className='fixed z-40 bg-bg-main  w-full'>
-              <Header/>
-            </div>
-          </div>
-        <div className="relative z-10 space-y-16 ">
-          <TitleSection/>
-          <StatdiumSection/>
-          <WhatWeDoSection/>
-          <TokenSection/>   
-          <AssetSection/>
-          <PartnerSection/>
+  return isMobile ? (
+    <>
+      <div className="flex justify-center w-full h-[60px]">
+        <div className="fixed z-40 bg-bg-main  w-full">
+          <Header />
         </div>
-        <div className="z-10 w-full  border-y  border-[#272A2A]">
-          <GameSection/>
-        </div>
+      </div>
+      <div className="relative z-10 space-y-16 ">
+        <TitleSection />
+        <StatdiumSection />
+        <WhatWeDoSection />
+        <TokenSection />
+        <AssetSection />
+        <PartnerSection />
+      </div>
+      <div className="z-10 w-full  border-y  border-[#272A2A]">
+        <GameSection />
+      </div>
       <div className="z-10 w-full">
-        <Footer/>
+        <Footer />
       </div>
     </>
-    :<>
-    <div className='flex justify-center w-full
-                    md:h-[60px] xl:h-[72px]'>
-      <div className='fixed z-40 bg-bg-main
+  ) : (
+    <>
+      <div
+        className="flex justify-center w-full
+                    md:h-[60px] xl:h-[72px]"
+      >
+        <div
+          className="fixed z-40 bg-bg-main
                       2xl:w-[1440px] xl:w-[1280px] lg:w-[1024px] 
-                      h-[60px] xl:h-[73px] border-b border-[#272A2A] '>
-        <Header/>
+                      h-[60px] xl:h-[73px] border-b border-[#272A2A] "
+        >
+          <Header />
+        </div>
       </div>
-    </div>
 
-    <div className="z-10 px-6">
-      <TitleSection/>
-      {/* cylinder section */}
-      <StatdiumSection/>
-      {/* whatwedo section */}
-      <WhatWeDoSection/>
+      <div className="z-10 px-6">
+        <TitleSection />
+        {/* cylinder section */}
+        <StatdiumSection />
+        {/* whatwedo section */}
+        {/* <WhatWeDoSection/> */}
       </div>
       <div className="z-10 ">
-
-      {/* token section */}
-      <TokenSection/>
-      {/* secure section */}
-      <AssetSection/>
-      {/* partners section */}
-      <PartnerSection/>
-      {/* news&update section */}
-      {/* [Auxili's Request] <NewsSection/>*/}
-      {/* game section */}
-      <GameSection/>
-      {/* footer section */}
-      <Footer/>
+        {/* token section */}
+        <TokenSection />
+        {/* secure section */}
+        <AssetSection />
+        {/* partners section */}
+        <PartnerSection />
+        {/* news&update section */}
+        {/* [Auxili's Request] <NewsSection/>*/}
+        {/* game section */}
+        <GameSection />
+        {/* footer section */}
+        <Footer />
       </div>
-
     </>
-  )
+  );
 }
