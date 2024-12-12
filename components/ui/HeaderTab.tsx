932d1e5c9998bc2cee90feeea44c6c764ea716d2
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2024-12-12 15:40:25 +0900

    preslae backgroudn stipes fix

--- components/ui/HeaderTab.tsx
+++ components/ui/HeaderTab.tsx
@@ -14,10 +14,7 @@ export function HeaderTab({
   return (
     <div className="relative w-full h-full text-[15px] lg:text-[20px] leading-[28px] tracking-widest">
       <div className="w-full h-full border-transparent  border-b-2 border-[#272A2A] hover:border-b-2 hover:border-jala-red duration-500">
-        <Link
-          className="w-full h-full flex justify-center items-center space-x-2"
-          href={href}
-        >
+        <Link className="w-full h-full flex justify-center items-center space-x-2" href={href}>
           {backgrounded && (
             <img
               className="absolute bottom-0 left-0 right-0 top-0 h-full w-full object-cover"
@@ -25,7 +22,7 @@ export function HeaderTab({
             />
           )}
           {!!imgUrl && <img className="h-4 xl:h-4" src={imgUrl} />}
-          <div>{title}</div>
+          <div className="z-10">{title}</div>
         </Link>
       </div>
     </div>
